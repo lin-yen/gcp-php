de07bd8a2af43098e9967074f23f5e3cfafb39ee
lin-yen <s811214studen@gmail.com>
2018-01-11 14:50:18 +0800

    use go service NAME

--- src/getHelloGo.php
+++ src/getHelloGo.php
@@ -2,8 +2,8 @@
   // creat curl connect
   $ch = curl_init();
 
-  // set url，內部互打用 go 的 container 名稱
-  curl_setopt($ch, CURLOPT_URL, "http://go:4000/say-hello");
+  // set url，內部互打用 go 的 container NAME (gcp service NAME)
+  curl_setopt($ch, CURLOPT_URL, "http://go-service:4000/say-hello");
   curl_setopt($ch, CURLOPT_HEADER, false);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
